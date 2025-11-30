3e645e9db1bf26655c5e7ddb89c6c8dfc69939c4
isroilsaparboev-png <isroilsaparboev@gmail.com>
2025-11-29 17:54:35 -0800

    masala ishlash
    c++ dasturdan masala ishladim

--- c++_16_dars.py
+++ c++_16_dars.py
@@ -1,5 +1,13 @@
-k = int(input())
-n = int(input())
-for i in range(n):
-  print(k)
-  
+#1 k = int(input())
+#n = int(input())
+#for i in range(n):
+#  print(k)
+#2a = int(input())
+#b = int(input())
+#count = 0
+#for i in range(a, b + 1):
+#    print(i)
+#    count += 1
+#    print("Soni =", count)
+
+
